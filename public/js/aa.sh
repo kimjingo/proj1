8ed00d25d2cada7e366b2e802c77691b731fdfd7
jay <kimjingo@gmail.com>
2018-01-05 20:09:05 -0800

    css
    update css for bootstrap

---
+++ public/js/aa.sh
@@ -0,0 +1,8 @@
+wget http://getbootstrap.com/2.3.2/assets/css/bootstrap.css
+wget http://getbootstrap.com/2.3.2/assets/css/bootstrap-responsive.css
+wget http://getbootstrap.com/2.3.2/assets/js/html5shiv.js
+wget http://getbootstrap.com/2.3.2/assets/ico/apple-touch-icon-144-precomposed.png
+wget http://getbootstrap.com/2.3.2/assets/ico/apple-touch-icon-114-precomposed.png
+wget http://getbootstrap.com/2.3.2/assets/ico/apple-touch-icon-72-precomposed.png
+wget http://getbootstrap.com/2.3.2/assets/ico/apple-touch-icon-57-precomposed.png
+wget http://getbootstrap.com/2.3.2/assets/ico/favicon.png
